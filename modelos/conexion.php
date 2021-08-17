c6e6ab4ab991185fa1abb6723925d330cafedb8d
Fernando <gatoguevara2@hotmail.com>
2021-08-17 12:26:17 -0500

    conexion

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -6,7 +6,7 @@ class Conexion
     public static function conectar()
     {
 
-        $link = new PDO("mysql:host=localhost;dbname=backend_mvc", "root", "", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        $link = new PDO("mysql:host=bboikui4rpocyu8jk3uu-mysql.services.clever-cloud.com;dbname=bboikui4rpocyu8jk3uu", "uut0lbezm3digd2u", "hsKoKwzN20ktiyJGSrQQ", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::MYSQL_ATTR_INIT_COMMAND                                                                   => "SET NAMES utf8"));
         return $link;
     }
